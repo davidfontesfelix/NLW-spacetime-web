854166f4cb851248aa6688274e71df041cdd9248
davidfontesfelix <davidfontes303@gmail.com>
2023-06-12 21:45:18 -0300

    NLW-Front-end-web

---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
+
+export default function Hero() {
+  return (
+    <div className="space-y-5">
+      <Image src={nlwLogo} alt="NLW spacetime" />
+
+      <div className="max-w-[420px] space-y-4">
+        <h1 className="text-5xl font-bold leading-tight text-gray-50">
+          Sua cápsula do tempo
+        </h1>
+        <p className="text-lg leading-relaxed">
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+          com o mundo!
+        </p>
+      </div>
+      <Link
+        href="/memories/new"
+        className="inline-block cursor-pointer rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
+      >
+        CADASTRAR LEMBRANÇA
+      </Link>
+    </div>
+  )
+}
---
+++ src/components/MediaPicker.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { ChangeEvent, useState } from 'react'
+
+export default function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewURl = URL.createObjectURL(files[0])
+
+    setPreview(previewURl)
+  }
+  return (
+    <>
+      <input
+        onChange={onFileSelected}
+        type="file"
+        id="media"
+        name="coverURL"
+        accept="image/*"
+        className="invisible h-0 w-0"
+      />
+
+      {preview && (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </>
+  )
+}
